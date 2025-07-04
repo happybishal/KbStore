26487a01089895e589644230c0cc7ab7535f0e67
happybishal <bishalsenchuri101@gmail.com>
2025-07-04 14:49:24 +0900

    Add STATIC_ROOT for static files collection

--- beautyshop/settings.py
+++ beautyshop/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -123,3 +125,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
